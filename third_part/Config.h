d32957227f36767dda42a6c608b674917ea16a53
luoyi <844262725@qq.com>
2016-04-02 23:00:20 +0800

    add third_part

---
+++ third_part/Config.h
@@ -0,0 +1,27 @@
+/*=================================================================
+#
+# Copyright 2016-2020
+#
+# Author: luoyi 844262725@qq.com
+#
+# Last modified: 2016-02-17 22:25
+#
+# Filename: Config.h
+#
+# Description: 
+#
+==================================================================*/
+#ifndef WIND7_CONFIG_H_ 
+#define WIND7_CONFIG_H_
+
+#include <OGRE/OgrePlatform.h>
+
+#   if defined(_DEBUG) || defined (DEBUG)
+#       define WIND_DEBUG 1
+#   else
+#       define WIND_DEBUG 0
+#   endif 
+
+#   define WIND_EXPORT _OgreExport
+
+#endif /*WIN7_CONFIG_H_*/
